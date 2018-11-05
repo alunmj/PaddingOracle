cd880ed205c6b24d3826cf4aab8094cf01120e42
alunmj <alun@texis.com>
2018-11-04 18:18:01 -0800

    Refactored and added command line arguments
    Refactored to move stuff out of the "Program.cs" file - that should really only be about parsing the command line arguments, and outputting results.
    Added command line arguments, so you don't have to edit the config file if running against different targets.
    Added the encrypt functionality, for challenges requiring you to encrypt data.
    Support blocksizes up to 32 bytes (untested - someone want to try them?)

---
+++ PadOracle/FrequencyTrials.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PadOracle
+{
+    class FrequencyTrials
+    {
+        public static IEnumerable<byte> Sequence => Enumerable.Range(0, 256).Select(x => (byte)x);
+    }
+    class PaddingFrequency32 : FrequencyTrials
+    {
+        static private byte[] baseSequence = new byte[]
+        {
+            0x20,0x1f,0x1e,0x1d,0x1c,0x1b,0x1a,0x19,0x18,0x17,0x16,0x15,0x14,0x13,0x12,0x11,
+            0x10,0x0f,0x0e,0x0d,0x0c,0x0b,0x0a,0x09,0x08,0x07,0x06,0x05,0x04,0x03,0x02,0x01
+        };
+        public static new IEnumerable<byte> Sequence => baseSequence;
+    }
+    class EnglishFrequency : FrequencyTrials
+    {
+        static private byte[] baseSequence = null;
+        public static new IEnumerable<byte> Sequence
+        {
+            get
+            {
+                if (baseSequence == null)
+                {
+                    baseSequence = new byte[256];
+                    string charfreq = "etaonishrlducmwyfgpbvkjxqzETAONISHRLDUCMWYFGPBVKJXQZ,. -'_?:()!0123456789\n\r{}~/\"^$";
+                    UTF8Encoding.UTF8.GetBytes(charfreq).CopyTo(baseSequence, 0);
+                    int j = charfreq.Length;
+                    // Fill in the rest of the bytes, in case there's a character we didn't think of.
+                    for (int i = 0; i < 256; i++)
+                    {
+                        if (!charfreq.Contains((char)i))
+                        {
+                            baseSequence[j++] = (byte)i;
+                        }
+                    }
+                    // We got all the bytes exactly once, yes?
+                    System.Diagnostics.Debug.Assert(j == 256);
+
+                }
+                return baseSequence;
+            }
+        }
+    }
+}
--- PadOracle/Properties/Settings.Designer.cs
+++ PadOracle/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace PadOracle.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.7.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.8.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -20,24 +20,6 @@ namespace PadOracle.Properties {
         public static Settings Default {
             get {
                 return defaultInstance;
-            }
-        }
-        
-        [global::System.Configuration.ApplicationScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("localhost:31140")]
-        public string hostName {
-            get {
-                return ((string)(this["hostName"]));
-            }
-        }
-        
-        [global::System.Configuration.ApplicationScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("16")]
-        public int blockSize {
-            get {
-                return ((int)(this["blockSize"]));
             }
         }
     }
